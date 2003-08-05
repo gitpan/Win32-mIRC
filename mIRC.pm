a464d0ff7721fc44ea1716d7f0bee740daf19917
Matthew Musgrove <mmusgrove@cpan.org>
2003-08-05 16:32:15 +0000

    Import of MMUSGROVE/Win32-mIRC-0.03a from CPAN.
    gitpan-cpan-distribution: Win32-mIRC
    gitpan-cpan-version:      0.03a
    gitpan-cpan-path:         MMUSGROVE/Win32-mIRC-0.03a.tar.gz
    gitpan-cpan-author:       MMUSGROVE
    gitpan-cpan-maturity:     released

--- mIRC.pm
+++ mIRC.pm
@@ -6,7 +6,7 @@ use warnings;
 BEGIN {
 	use Exporter();
 	use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $dde);
-	$VERSION     = 0.02;
+	$VERSION     = 0.03;
 	@ISA         = qw(Exporter);
 	@EXPORT      = qw();
 	@EXPORT_OK   = qw(
